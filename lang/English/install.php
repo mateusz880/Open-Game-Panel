6712843ed331037faf222300bd13e8b21431d1cb
mateusz880 <mateusz880@radaczewo24.pl>
2014-10-22 01:39:23 +0200

    Update install.php

--- lang/English/install.php
+++ lang/English/install.php
@@ -22,52 +22,48 @@
  *
  */
 
-$lang['install_lang'] = "Wybierz preferowany język";
-$lang['install_welcome'] = "Witamy w Open game panel instalacji";
-$lang['file_permission_check'] = "Sprawdzanie uprawnień plików";
-$lang['OK'] = "Ok";
-$lang['write_permission_required'] = "Wymagane uprawnienia do zapisu";
-$lang['execute_permission_required'] = "Wymagane uprawnienia do wykonywania";
-$lang['file_is_missing'] = "Brakuje pliku";
-$lang['found'] = "znaleziono";
-$lang['not_found'] = "nie znaleziono";
+$lang['install_lang'] = "Select your preferred language";
+$lang['install_welcome'] = "Welcome to Open Game Panel setup";
+$lang['file_permission_check'] = "Checking required file permissions";
+$lang['OK'] = "OK";
+$lang['write_permission_required'] = "Write permission required";
+$lang['execute_permission_required'] = "Execute permission required";
+$lang['create_an_empty_file'] = "Create an empty file.";
+$lang['found'] = "Found";
+$lang['not_found'] = "Not Found";
 $lang['pear_xxtea_info'] = 'Pear Crypt_XXTEA is required for OGP usage. In most of the Linux systems this module can be installed with pear with the following command "pear install Crypt_XXTEA-beta".';
-$lang['refresh'] = "Odśwież";
-$lang['checking_required_modules'] = "Sprawdzanie wymaganych modułów";
-$lang['database_type'] = "Typ Bazy";
-$lang['database_settings'] = "Dane do bazy danzch";
-$lang['database_hostname'] = "Host bazy";
-$lang['database_username'] = "Nazwa użytkownika bazy";
-$lang['database_password'] = "Hasło bazy";
-$lang['database_name'] = "Nazwa bazy";
-$lang['database_prefix'] = "Prefix bazy";
-$lang['next'] = "Dalej";
-$lang['encryption_key'] = "Klucz szyfrowania (Agent)";
+$lang['refresh'] = "Refresh";
+$lang['checking_required_modules'] = "Checking required modules";
+$lang['database_type'] = "Database type";
+$lang['database_settings'] = "Database access settings";
+$lang['database_hostname'] = "Database Hostname";
+$lang['database_username'] = "Database Username";
+$lang['database_password'] = "Database Password";
+$lang['database_name'] = "Database Name";
+$lang['database_prefix'] = "Database Prefix";
+$lang['next'] = "Next";
+$lang['encryption_key'] = "Encyption key (Agent)";
 $lang['agent_port'] = "Port (Agent)";
-$lang['unable_to_write_config'] = "Nie można zapisać konfiguracji. Proszę sprawdzić uprawnienia do zapisu.";
-$lang['admin_login_details'] = "Dane konta admina";
-$lang['config_written'] = "Plik konfiguracyjny utworzony pomyślnie.";
-$lang['database_created'] = "Tabele w bazie danych utworzone pomyślnie.";
-$lang['admin_login_details_info'] = "Tworzymy konto admina w panelu.";
-$lang['username'] = "Użytkownik";
-$lang['password'] = "Hasło";
-$lang['repeat_password'] = "Powtórz hasło";
-$lang['email'] = "Adres E-mail";
-$lang['back'] = "z powrotem";
-$lang['database_setup_failure'] = "Instalator nie mógł utworzyć bazę danych. Proszę ponownie sprawdzić pliki konfiguracyjne bazy danych";
-$lang['php_version_check'] = "Sprawdzenie wersji PHP";
-$lang['invalid_username'] = "Nazwa użytkownika zawiera niedozwolone znaki.";
-$lang['password_too_short'] = "Twoje hasło jest za krótkie. Musi być co najmniej '%d' znaków.";
-$lang['password_contains_invalid_characters'] = "Hasło zawiera niedozwolone znaki.";
-$lang['invalid_email_address'] = "Adres E-mail jest nieprawidłowy.";
-$lang['setup_complete'] = "Instalacja kompletna";
-$lang['remove_install_and_secure_config'] = "Instalacja zakończona, panel jest gotowy.
-<a href='index.php'>Kliknij tutaj</a> aby przejść do panelu.
-<b>Uwaga:</b> należy usunąć plik install.php
-i ustawić chmod pliku config.inc.php na 644 w celu bezpieczeństwa.";
-$lang['go_to_panel'] = "Przejdź do panelu";
-$lang['unable_to_resolve'] = "Brak możliwości rozwiązania tego problemu. Zgłoś to na forum projektu.";
+$lang['unable_to_write_config'] = "Unable to write to config. Please recheck write permissions.";
+$lang['admin_login_details'] = "Admin login details";
+$lang['config_written'] = "The config files created successfully.";
+$lang['database_created'] = "Database tables created succesfully.";
+$lang['admin_login_details_info'] = "Now we create admin user for your Open Game Panel.";
+$lang['username'] = "Username";
+$lang['password'] = "Password";
+$lang['repeat_password'] = "Repeat password";
+$lang['email'] = "Email address";
+$lang['back'] = "Back";
+$lang['database_setup_failure'] = "Setup was unable to create the database. Please recheck your database configs.";
+$lang['php_version_check'] = "Checking PHP version.";
+$lang['invalid_username'] = "You entered invalid username.";
+$lang['password_too_short'] = "Your password is too short. It must be at least '%d' characters long.";
+$lang['password_contains_invalid_characters'] = "Your password contains invalid characters.";
+$lang['invalid_email_address'] = "You entered invalid email address.";
+$lang['setup_complete'] = "Setup has been completed successfully. The Open Game Panel is now ready for use.";
+$lang['remove_install_and_secure_config'] = "You should delete install.php from your server and chmod your includes/config.inc.php back to 644 for security purposes.";
+$lang['go_to_panel'] = "Click here to login to your OGP.";
+$lang['unable_to_resolve'] = "If you are unable to resolve this problem please visit OGP website ";
 $lang['slogan'] = "The OpenSource one!";
-$lang['welcome_title_message'] = "Witamy! <b style='font-size:12px; font-weight:normal;'>Edycja tytułu możesz dokonać tutaj'<a href='?m=settings&p=themes'>Ustawienia motywu</a>' w ramach '<a href='?m=administration&p=main'>Administracja</a>' zakładka.</b>";
-
+$lang['welcome_title_message'] = "Welcome! <b style='font-size:12px; font-weight:normal;'>You can change this title in '<a href='?m=settings&p=themes'>Theme Settings</a>' under the '<a href='?m=administration&p=main'>Administration</a>' tab.</b>";
 ?>
