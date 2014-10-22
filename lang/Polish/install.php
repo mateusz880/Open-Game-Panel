5401446d5fd8b7918a08cf6f5c5872c71551cf4e
mateusz880 <mateusz880@radaczewo24.pl>
2014-10-22 11:27:32 +0200

    Update install.php

--- lang/Polish/install.php
+++ lang/Polish/install.php
@@ -1,73 +1,73 @@
 <?php
 /*
-*
-* Copyright (C) 2008 OGP Team
-*
-* www.opengamepanel.org
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) Copyright (C) 2008 - 2013 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 
-$lang['php_version_check'] = "Sprawdzenie wersji PHP";
 $lang['install_lang'] = "Wybierz preferowany język";
 $lang['install_welcome'] = "Witamy w Open game panel instalacji";
-$lang['file_permission_check'] = "Sprawdzanie wymaganych uprawnień pliku";
-$lang['OK'] = "OK";
-$lang['write_permission_required'] = "Write permission required";
-$lang['execute_permission_required'] = "Execute permission required";
-$lang['file_is_missing'] = "File is missing.";
-$lang['found'] = "found";
-$lang['not_found'] = "not found";
-$lang['checking_required_modules'] = "Checking required modules";
-$lang['database_settings'] = "Database access settings";
-$lang['database_hostname'] = "Database Hostname";
-$lang['database_username'] = "Database Username";
-$lang['database_password'] = "Database Password";
-$lang['database_name'] = "Database Name";
+$lang['file_permission_check'] = "Sprawdzanie uprawnień plików";
+$lang['OK'] = "Ok";
+$lang['write_permission_required'] = "Wymagane uprawnienia do zapisu";
+$lang['execute_permission_required'] = "Wymagane uprawnienia do wykonywania";
+$lang['file_is_missing'] = "Brakuje pliku";
+$lang['found'] = "znaleziono";
+$lang['not_found'] = "nie znaleziono";
+$lang['pear_xxtea_info'] = 'Pear Crypt_XXTEA is required for OGP usage. In most of the Linux systems this module can be installed with pear with the following command "pear install Crypt_XXTEA-beta".';
+$lang['refresh'] = "Odśwież";
+$lang['checking_required_modules'] = "Sprawdzanie wymaganych modułów";
+$lang['database_type'] = "Typ Bazy";
+$lang['database_settings'] = "Dane do bazy danzch";
+$lang['database_hostname'] = "Host bazy";
+$lang['database_username'] = "Nazwa użytkownika bazy";
+$lang['database_password'] = "Hasło bazy";
+$lang['database_name'] = "Nazwa bazy";
+$lang['database_prefix'] = "Prefix bazy";
 $lang['next'] = "Dalej";
-$lang['encryption_key'] = "Encyption key (Agent)";
+$lang['encryption_key'] = "Klucz szyfrowania (Agent)";
 $lang['agent_port'] = "Port (Agent)";
-$lang['unable_to_write_config'] = "Unable to write to config. Please recheck write permissions.";
-$lang['admin_login_details'] = "Admin login details";
-$lang['config_written'] = "The config files created successfully.";
-$lang['database_created'] = "Database tables created succesfully.";
-$lang['admin_login_details_info'] = "Now we create admin user for your Open game panel.";
-$lang['username'] = "Username";
-$lang['password'] = "Password";
-$lang['repeat_password'] = "Repeat password";
-$lang['email'] = "Email address";
-$lang['back'] = "Wróć";
-$lang['database_setup_failure'] = "Setup was unable to create the database. Please recheck your database configs.";
-
-$lang['password_too_short'] = "Your password is too short. It must be at least '%d' characters long.";
-
-$lang['unable_to_resolve'] = "If you are unable to resolve this problem please visit OGP website ";
-
-$lang['450'] = "User name contains invalid characters.";
-$lang['451'] = "Your two Passwords do not match.";
-$lang['452'] = "Invalid password, please use alpha numeric characters only.
-Password must be a minimum of 6 characters long.";
-$lang['453'] = "Your email address does not appear to be valid.";
-$lang['454'] = "There is an error in your user settings:";
-
-$lang['458'] = "You must delete install.php first before you can login.";
-$lang['580'] = "Setup is complete, game panel is now ready for use.
-<a href='index.php'>Click here</a> to go to your game panel.
-<b>Note:</b> you should delete install.php from your server.
-and chmod your config.inc.php back to 644 for security purposes.";
+$lang['unable_to_write_config'] = "Nie można zapisać konfiguracji. Proszę sprawdzić uprawnienia do zapisu.";
+$lang['admin_login_details'] = "Dane konta admina";
+$lang['config_written'] = "Plik konfiguracyjny utworzony pomyślnie.";
+$lang['database_created'] = "Tabele w bazie danych utworzone pomyślnie.";
+$lang['admin_login_details_info'] = "Tworzymy konto admina w panelu.";
+$lang['username'] = "Użytkownik";
+$lang['password'] = "Hasło";
+$lang['repeat_password'] = "Powtórz hasło";
+$lang['email'] = "Adres E-mail";
+$lang['back'] = "z powrotem";
+$lang['database_setup_failure'] = "Instalator nie mógł utworzyć bazę danych. Proszę ponownie sprawdzić pliki konfiguracyjne bazy danych";
+$lang['php_version_check'] = "Sprawdzenie wersji PHP";
+$lang['invalid_username'] = "Nazwa użytkownika zawiera niedozwolone znaki.";
+$lang['password_too_short'] = "Twoje hasło jest za krótkie. Musi być co najmniej '%d' znaków.";
+$lang['password_contains_invalid_characters'] = "Hasło zawiera niedozwolone znaki.";
+$lang['invalid_email_address'] = "Adres E-mail jest nieprawidłowy.";
+$lang['setup_complete'] = "Instalacja kompletna";
+$lang['remove_install_and_secure_config'] = "Instalacja zakończona, panel jest gotowy.
+<a href='index.php'>Kliknij tutaj</a> aby przejść do panelu.
+<b>Uwaga:</b> należy usunąć plik install.php
+i ustawić chmod pliku config.inc.php na 644 w celu bezpieczeństwa.";
+$lang['go_to_panel'] = "Przejdź do panelu";
+$lang['unable_to_resolve'] = "Brak możliwości rozwiązania tego problemu. Zgłoś to na forum projektu.";
 $lang['slogan'] = "The OpenSource one!";
-$lang['welcome_title_message'] = "Welcome! <b style='font-size:12px; font-weight:normal;'>You can change this title in '<a href='?m=settings&p=themes'>Theme Settings</a>' under the '<a href='?m=administration&p=main'>Administration</a>' tab.</b>";
+$lang['welcome_title_message'] = "Witamy! <b style='font-size:12px; font-weight:normal;'>Edycja tytułu możesz dokonać tutaj'<a href='?m=settings&p=themes'>Ustawienia motywu</a>' w ramach '<a href='?m=administration&p=main'>Administracja</a>' zakładka.</b>";
 
 ?>
